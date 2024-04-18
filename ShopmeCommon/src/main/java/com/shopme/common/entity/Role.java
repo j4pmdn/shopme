2babcd136c15b601e67c3b08cd61895c71ab7870
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-04-18 22:35:07 +0700

    Code User Entity, User Repository and Unit Tests

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -1,5 +1,7 @@
 package com.shopme.common.entity;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,6 +24,10 @@ public class Role {
 	private String description;
 	
 	public Role() {
+	}
+	
+	public Role(Integer id) {
+		this.id = id;
 	}
 	
 	public Role(String name) {
@@ -52,6 +58,28 @@ public class Role {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", name=" + name + ", description=" + description + "]";
+	}
+
 }
