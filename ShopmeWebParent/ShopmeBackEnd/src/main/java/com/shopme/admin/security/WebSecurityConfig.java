f97b785022ff79e58db201b0ce5b57daeb29173d
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-04-25 10:19:52 +0700

    Code user login page

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -2,13 +2,23 @@ package com.shopme.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+	@Bean
+    UserDetailsService userDetailsService() {
+        return new ShopmeUserDetailsService();
+    }
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -16,12 +26,34 @@ public class WebSecurityConfig {
 	}
 	
 	@Bean
-	SecurityFilterChain configuSecurityFilterChain(HttpSecurity http) throws Exception {
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        
+        return authProvider;
+    }
+	
+	@Bean
+	SecurityFilterChain configureHttp(HttpSecurity http) throws Exception {
+		http.authenticationProvider(authenticationProvider());
 		
-		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-		
+		http.authorizeHttpRequests(auth -> auth
+		        .anyRequest().authenticated()
+		        )
+		        .formLogin(form -> form
+		                .loginPage("/login")
+		                .usernameParameter("email")
+		                .permitAll()
+		        );
+				
 		return http.build();
 		
+	}
+	
+	@Bean
+	WebSecurityCustomizer configWebsecurity() throws Exception {
+		return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
 	}
 	
 }
