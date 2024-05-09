837d929eab516641cd95cd3a635783a36c2aefe7
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-05-10 06:47:44 +0700

    Code delete category function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtil.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtil.java
@@ -51,4 +51,15 @@ public class FileUploadUtil {
 		}
 	}
 	
+	public static void removeDir(String dir) {
+		cleanDir(dir);
+
+		try {
+			Files.delete(Paths.get(dir));
+		} catch (IOException e) {
+			LOGGER.error("Could not remove directory: " + dir);
+		}
+
+	}
+	
 }
