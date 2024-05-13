e8439dafad27051a7b9d5ce53cbc295110f2d905
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-05-14 06:24:56 +0700

    Code product overview form

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -1,13 +1,16 @@
 package com.shopme.admin.brand;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.shopme.common.entity.Brand;
 
-public interface BrandRepository extends CrudRepository<Brand, Integer> {
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
 	
 	public Long countById(Integer id);
 	
@@ -16,6 +19,8 @@ public interface BrandRepository extends CrudRepository<Brand, Integer> {
 	@Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
 	public Page<Brand> findAll(String keyword, Pageable pageable);
 	
-	public Page<Brand> findAll(Pageable pageable);
+	@Query("SELECT NEW Brand(b.id, b.name) FROM Brand b ORDER BY b.name ASC")
+	public List<Brand> findAll();
+	
 
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.shopme.admin.brand.BrandService;
+import com.shopme.common.entity.Brand;
 import com.shopme.common.entity.Product;
 
 @Controller
@@ -15,6 +18,9 @@ public class ProductController {
 	@Autowired 
 	private ProductService productService;
 	
+	@Autowired 
+	private BrandService brandService;
+	
 	@GetMapping("/products")
 	public String listAll(Model model) {
 		List<Product> listProducts = productService.listAll();
@@ -22,6 +28,30 @@ public class ProductController {
 		model.addAttribute("listProducts", listProducts);
 		
 		return "products/products";
+	}
+	
+	@GetMapping("/products/new")
+	public String newProduct(Model model) {
+		List<Brand> listBrands = brandService.listAll();
+		
+		Product product = new Product();
+		product.setEnabled(true);
+		product.setInStock(true);
+		
+		model.addAttribute("product", product);
+		model.addAttribute("listBrands", listBrands);
+		model.addAttribute("pageTitle", "Create New Product");
+		
+		return "products/product_form";
+	}
+	
+	@PostMapping("/products/save")
+	public String saveProduct(Product product) {
+		System.out.println("Product Name: " + product.getName());
+		System.out.println("Brand ID: " + product.getBrand().getId());
+		System.out.println("Category ID: " + product.getCategory().getId());
+
+		return "redirect:/products";
 	}
 
 }
