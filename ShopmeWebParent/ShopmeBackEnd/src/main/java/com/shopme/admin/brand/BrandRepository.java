135656fcb12c68ac7492ab2ec98d06dbd69eeb6f
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-05-13 08:02:26 +0700

    code brand entity, repository, unitest, list brands, brand form, category selection on form

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.shopme.admin.brand;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.shopme.common.entity.Brand;
+
+public interface BrandRepository extends CrudRepository<Brand, Integer>{
+
+}
