0b8f10020b1e47183afc948c5a0ec3bfc610fc14
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-05-17 01:09:52 +0700

    Code setting entity, repository and unit tests

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingRepository.java
@@ -0,0 +1,11 @@
+package com.shopme.admin.setting;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.shopme.common.entity.Setting;
+
+public interface SettingRepository extends CrudRepository<Setting, String> {
+	
+	
+	
+}
